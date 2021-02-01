4fda45cb621958113179d4733507d181644d99e2
Natalia Koc <natalia-koc@o2.pl>
2021-02-01 02:47:28 +0100

    usuniecie spacji

--- src/main/java/natalia/koc/sklepZoologiczny/domain/Historia.java
+++ src/main/java/natalia/koc/sklepZoologiczny/domain/Historia.java
@@ -3,7 +3,6 @@ package natalia.koc.sklepZoologiczny.domain;
 import com.sun.istack.NotNull;
 import lombok.Getter;
 import lombok.Setter;
-
 import javax.persistence.*;
 import java.time.LocalDate;
 
